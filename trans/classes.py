1756bb2e7cd65a7b7d02426820c7ddfbce72134b
Kieran Colford <colfordk@gmail.com>
2014-09-20 06:23:04 -0400

    added variables to the id lookups

--- trans/classes.py
+++ trans/classes.py
@@ -1,5 +1,5 @@
 import translator
-import symtable
+import ast
 
 
 class Classes(translator.Translator):
@@ -7,17 +7,17 @@ class Classes(translator.Translator):
     """
 
     def visit_ClassDef(self, node):
-        sym = symtable.symtable(node, "", "exec")
         ret = ""
         ret += "function _Py2JS_" + node.name + "() {"
         ret += ' '.join(self.visit(i) for i in node.body)
-        for i in sym.get_identifiers():
+        for i in get_ids(node):
             ret += "this." + i + " = " + i + ";"
         ret += "}"
         ret += node.name + " = type.__call__(" + node.name + ", ["
         ret += ', '.join(self.visit(i) for i in node.bases)
         ret += "], new _Py2JS_" + node.name + "()"
         ret += ")"
+        return ret
 
 class Modules(translator.Translator):
     """This translates module definitions into java script.
@@ -31,3 +31,23 @@ class Modules(translator.Translator):
         for i in sym.get_identifiers():
             ret += "this." + i + " = " + i + ";"
         ret += "}"
+        return ret
+
+def get_ids(node):
+    """Get the identifiers in an AST object.
+    """
+
+    class walker(ast.NodeVisitor):
+        out = []
+        def __call__(self, node):
+            self.visit(node)
+            return self.out
+        def visit_FunctionDef(self, node):
+            self.out.append(node.name)
+        def visit_Assign(self, node):
+            self.visit(node.targets)
+        def visit_Name(self, node):
+            self.out.append(node.id)
+    
+    return walker()(node)
+
