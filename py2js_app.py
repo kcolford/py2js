dd15affca29ea5c23e3b5fc1006f9f7c908e7e18
Li Xuanji <xuanji@gmail.com>
2014-09-20 13:20:18 +0800

    spaces

--- py2js_app.py
+++ py2js_app.py
@@ -4,16 +4,16 @@ import webapp2
 from trans import translate
 
 class Translate(webapp2.RequestHandler):
-  def post(self):
-    self.response.headers['Content-Type'] = 'text/plain'
-    python_string = self.request.get('python_string')
-    self.response.write(translate(python_string))
+    def post(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        python_string = self.request.get('python_string')
+        self.response.write(translate(python_string))
 
 class TestTranslate(webapp2.RequestHandler):
-  def get(self):
+    def get(self):
 
-    f = open('py2js/test_translate.html')
-    self.response.write(f.read())
+        f = open('py2js/test_translate.html')
+        self.response.write(f.read())
 
 app = webapp2.WSGIApplication([('/py2js/translate/', Translate),
                                ('/py2js/test_translate/', TestTranslate)
