d4b8a6327cde00806e598dfbbd89ea8c5628671b
Kieran Colford <colfordk@gmail.com>
2014-09-20 01:47:12 -0400

    added print

--- trans/statement.py
+++ trans/statement.py
@@ -7,3 +7,10 @@ class Statement(translator.Translator):
 
     def visit_Assert(self, node):
         pass
+
+    def visit_Assign(self, node):
+        return (self.visit(node.targets) + " = " +
+                self.visit(node.value) + ';')
+    def visit_Print(self, node):
+        return ("console.log(" + ', '.join(self.visit(i) for i in node.values)
+                + ");")
