424c55ce33dcbcf122a196779adcc9e26c09e864
Li Xuanji <xuanji@gmail.com>
2014-09-20 12:45:13 +0800

    create translate and test endpoints

--- py2js_app.py
+++ py2js_app.py
@@ -1,15 +1,33 @@
 import logging
 import webapp2
 
+from trans import translate
+
 out = """Hi there!
 
 %s
 
 """
-class Echo(webapp2.RequestHandler):
-  def get(self):
-    self.response.headers['Content-Type'] = 'text/plain'
-    self.response.write(out % (self.request))
 
-app = webapp2.WSGIApplication([('/py2js/.*', Echo)],
+class Translate(webapp2.RequestHandler):
+  def get(self):
+    self.response.headers['Content-Type'] = 'text/plain' #todo: use json
+    self.response.write(translate('yo'))
+
+class TestTranslate(webapp2.RequestHandler):
+  def get(self):
+    self.response.write("""
+
+<html>
+<title> py2js </title>
+<body>
+Hi there
+</body
+</html>
+
+""")
+
+app = webapp2.WSGIApplication([('/py2js/translate/', Translate),
+                               ('/py2js/test_translate/', TestTranslate)
+                              ],
                               debug=True)
